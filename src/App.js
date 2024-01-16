a0eb9037e42749b02ff88b50997f12fd938b3617
Hamza-managements <148950006+Hamza-managements@users.noreply.github.com>
2024-01-17 04:51:10 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import Router from './config/router'
-
-import './App.css'
+import './App.css';
+import Router from './Config/router';
 
 function App() {
   return (
     <div className="App">
       <Router />
     </div>
-  )
+  );
 }
 
 export default App;
-
-/*
-  SPA vs MPA
-  SPA: Single Page Application
-  MPA: Multi Page Application
-
-*/
